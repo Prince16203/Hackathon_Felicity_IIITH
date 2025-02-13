ca4bbdf0aa9f9fe2b8b6e4afcb48983ade98b7a6
Prince16203 <stevestarc16@gmail.com>
2025-02-13 22:14:04 +0530

    TOOL BAR

--- frontend/src/pages/content/Comp1.jsx
+++ frontend/src/pages/content/Comp1.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import "../styles/Comp1.css"
 
 const comp1 = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-            <li>HOME</li>
+    <div className='div1'>  
+        <nav>
+          <ul>
+            <li>HOME</li> 
             <li>ABOUT US</li>
             <li>OUTREACH PORTAL</li>
             <li>PARTICIPATING INSTITUTES</li>
             <li>NMEICT</li>
             <li>CONTACT US</li>
-        </ul>
-      </nav>
+          </ul>
+        </nav>
+        
     </div>
   )
 }
