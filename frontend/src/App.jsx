9478f516874559b0b6ead8d7fc9c760e72d6afe3
Prince16203 <stevestarc16@gmail.com>
2025-02-13 22:40:53 +0530

    HEADER

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 
 
 import Comp1 from "./pages/content/Comp1"
+import Header from "./pages/content/Header"
 function App  ()  {
   return ( 
+    <div>
+      <Header/>
       <Comp1/>
+      
+    </div>
+      
   )
 }
 
---
+++ frontend/src/pages/content/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="text">
+        <p className="initiative">An Initiative of</p>
+        <p className="ministry"><strong>Ministry of Education</strong></p>
+        <p className="mission">
+          Under the National Mission on Education through <span className="ict">ICT</span>
+        </p>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search Lab" />
+        <button>🔍</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
